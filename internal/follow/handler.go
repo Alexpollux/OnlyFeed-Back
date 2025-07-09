ce25cc7b69d8bfb63028ef62b4d8dc3c77371747
arthurdelaporte <arthur.delaporte@eemi.com>
2025-07-09 16:00:27 +0200

    ajout logs follow
    Signed-off-by: arthurdelaporte <arthur.delaporte@eemi.com>

--- internal/follow/handler.go
+++ internal/follow/handler.go
@@ -1,6 +1,8 @@
 package follow
 
 import (
+	"fmt"
+	"github.com/ArthurDelaporte/OnlyFeed-Back/internal/logs"
 	"net/http"
 
 	"github.com/google/uuid"
@@ -12,11 +14,17 @@ import (
 
 // FollowUser POST /api/follow/:id
 func FollowUser(c *gin.Context) {
+	route := c.FullPath()
+
 	followerID := c.GetString("user_id")
 	followingID := c.Param("id")
 
 	if followerID == followingID {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Impossible de se suivre soi-même"})
+		logs.LogJSON("WARN", "Impossible to follow yourself", map[string]interface{}{
+			"route":  route,
+			"userID": followerID,
+		})
 		return
 	}
 
@@ -25,6 +33,11 @@ func FollowUser(c *gin.Context) {
 		Where("follower_id = ? AND creator_id = ?", followerID, followingID).
 		First(&existing).Error; err == nil {
 		c.JSON(http.StatusConflict, gin.H{"error": "Déjà suivi"})
+		logs.LogJSON("WARN", "Already followed", map[string]interface{}{
+			"route":  route,
+			"userID": followerID,
+			"extra":  fmt.Sprintf("followingID : %s", followingID),
+		})
 		return
 	}
 
@@ -36,14 +49,27 @@ func FollowUser(c *gin.Context) {
 
 	if err := database.DB.Create(&newFollow).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erreur ajout du follow", "details": err.Error()})
+		logs.LogJSON("ERROR", "Error adding follow", map[string]interface{}{
+			"error":  err.Error(),
+			"route":  route,
+			"userID": followerID,
+			"extra":  fmt.Sprintf("followingID : %s", followingID),
+		})
 		return
 	}
 
-	c.JSON(http.StatusCreated, gin.H{"message": "Utilisateur suivi ✅"})
+	c.JSON(http.StatusCreated, gin.H{"message": "Utilisateur suivi"})
+	logs.LogJSON("INFO", "Followed user", map[string]interface{}{
+		"route":  route,
+		"userID": followerID,
+		"extra":  fmt.Sprintf("followingID : %s", followingID),
+	})
 }
 
 // UnfollowUser DELETE /api/follow/:id
 func UnfollowUser(c *gin.Context) {
+	route := c.FullPath()
+
 	followerID := c.GetString("user_id")
 	followingID := c.Param("id")
 
@@ -52,14 +78,27 @@ func UnfollowUser(c *gin.Context) {
 		Where("follower_id = ? AND creator_id = ?", followerID, followingID).
 		Delete(&Follow{}).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erreur unfollow"})
+		logs.LogJSON("ERROR", "Error unfollow", map[string]interface{}{
+			"error":  err.Error(),
+			"route":  route,
+			"userID": followerID,
+			"extra":  fmt.Sprintf("followingID : %s", followingID),
+		})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Utilisateur unfollow ✅"})
+	c.JSON(http.StatusOK, gin.H{"message": "Utilisateur unfollow"})
+	logs.LogJSON("INFO", "User unfollow", map[string]interface{}{
+		"route":  route,
+		"userID": followerID,
+		"extra":  fmt.Sprintf("followingID : %s", followingID),
+	})
 }
 
 // GetFollowing GET /api/following
 func GetFollowing(c *gin.Context) {
+	route := c.FullPath()
+
 	followerID := c.GetString("user_id")
 
 	var follows []Follow
@@ -67,6 +106,11 @@ func GetFollowing(c *gin.Context) {
 		Where("follower_id = ?", followerID).
 		Find(&follows).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erreur récupération creator"})
+		logs.LogJSON("ERROR", "Error recovery creator", map[string]interface{}{
+			"error":  err.Error(),
+			"route":  route,
+			"userID": followerID,
+		})
 		return
 	}
 
@@ -77,15 +121,26 @@ func GetFollowing(c *gin.Context) {
 	}
 
 	if err := database.DB.Where("id IN ?", ids).Find(&usersFollowed).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erreur récupération users suivis"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erreur récupération des utilisateurs suivis"})
+		logs.LogJSON("ERROR", "Error retrieving followed users", map[string]interface{}{
+			"error":  err.Error(),
+			"route":  route,
+			"userID": followerID,
+		})
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"following": usersFollowed})
+	logs.LogJSON("INFO", "Recovering the list of users followed", map[string]interface{}{
+		"route":  route,
+		"userID": followerID,
+	})
 }
 
 // GetFollowers GET /api/followers/:id
 func GetFollowers(c *gin.Context) {
+	route := c.FullPath()
+
 	userID := c.Param("id")
 
 	var follows []Follow
@@ -93,6 +148,11 @@ func GetFollowers(c *gin.Context) {
 		Where("creator_id = ?", userID).
 		Find(&follows).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erreur récupération followers"})
+		logs.LogJSON("ERROR", "Error recovery followers", map[string]interface{}{
+			"error":  err.Error(),
+			"route":  route,
+			"userID": userID,
+		})
 		return
 	}
 
@@ -103,9 +163,18 @@ func GetFollowers(c *gin.Context) {
 	}
 
 	if err := database.DB.Where("id IN ?", ids).Find(&followers).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erreur récupération users followers"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erreur récupération des utilisateurs followers"})
+		logs.LogJSON("ERROR", "Error recovering followers", map[string]interface{}{
+			"error":  err.Error(),
+			"route":  route,
+			"userID": userID,
+		})
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"followers": followers})
+	logs.LogJSON("INFO", "Recovering the list of user followers", map[string]interface{}{
+		"route":  route,
+		"userID": userID,
+	})
 }
